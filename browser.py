806efd94a93022438cad054816dc36152a06c63e
ZuoyunZheng <zuoyunzheng@gmx.de>
2025-04-11 23:14:40 +0200

    added browser use

---
+++ browser.py
@@ -0,0 +1,50 @@
+from browser_use import Agent
+import asyncio
+
+async def main(llm):
+    task = f"Try apply for this job for the candidate: https://www.linkedin.com/jobs/view/4203903356. Avoid logging in to LinkedIn if you can and try to navigato the company's website to apply by clicking the apply button on the given linkedin link"
+    agent = Agent(
+        task=task,
+        llm=llm,
+    )
+    await agent.run()
+
+if __name__ == "__main__":
+    import pprint
+    from pathlib import Path
+    from typing import Annotated, Sequence
+
+    import pandas as pd
+    from dotenv import load_dotenv
+    from langchain import hub
+    from langchain.tools.retriever import create_retriever_tool
+    from langchain_core.messages import BaseMessage, HumanMessage
+    from langchain_ollama import ChatOllama
+    from langgraph.graph import END, START, StateGraph
+    from langgraph.graph.message import add_messages
+    from pydantic import BaseModel, Field
+    from typing_extensions import TypedDict
+
+    # from langgraph.prebuilt import create_react_agent
+    from rag import load_retriever
+    from utils.args import parse_args
+
+    load_dotenv()
+    args = parse_args()
+
+    llm = ChatOllama(
+        model="qwen2.5:1.5b",
+    )
+    retriever = load_retriever(args.reload_data)
+
+    retriever_llm = llm.bind_tools(
+        [
+            create_retriever_tool(
+                retriever,
+                "candidate_data_retriever",
+                "Contains candidate information, working & education experience, technical skills and qualifications",
+            )
+        ]
+    )
+
+    asyncio.run(main(retriever_llm))
---
+++ scripts/pw.py
@@ -0,0 +1,19 @@
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch()
+    page = browser.new_page()
+    page.goto("http://playwright.dev")
+    print(page.title())
+    browser.close()
+
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False, slow_mo=500)
+    page = browser.new_page()
+    page.goto("https://www.linkedin.com/jobs/view/4203903356")
+    page.screenshot(path="example.png")
+    import pdb; pdb.set_trace()
+    browser.close()
+
